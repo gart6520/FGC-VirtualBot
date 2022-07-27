b4cdc4d381785f11b77e2a5631acec01133931f6
Gabriel Ioannidis <gabkion@gmail.com>
2022-07-27 17:49:39 +0300

    Made a tank robot configuration for Carbon Capture

--- Controller/src/virtual_robot/game_elements/classes/Carbon.java
+++ Controller/src/virtual_robot/game_elements/classes/Carbon.java
@@ -14,6 +14,7 @@ import virtual_robot.dyn4j.Dyn4jUtil;
 import virtual_robot.dyn4j.FixtureData;
 import virtual_robot.games.CarbonCapture;
 
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,8 +29,9 @@ public class Carbon extends VirtualGameElement {
     public enum CarbonStatus {
         NORMAL(true, 100, CARBON_FILTER),
         FLYING(true, 0, CARBON_FLYING_FILTER),
-        ROLLING(true, 0.5, CARBON_FILTER),
+        ROLLING(true, 0.6, CARBON_FILTER),
         CONTROLLED(false, 0, CARBON_FILTER),
+        CAPTURED(true, 0.8, CARBON_CAPTURED_FILTER),
         OFF_FIELD(false, 0.0, CARBON_FILTER);   //Only these are available to human player
 
         private boolean inPlay;             // Is ring controlled by dyn4j physics world?
@@ -53,12 +55,14 @@ public class Carbon extends VirtualGameElement {
     BodyFixture carbonFixture;
 
     public static long CARBON_CATEGORY = 2048;
+    public static long CAPTURED_CARBON_CATEGORY = 4096;
 
     //Standard ring filter: collide with everything that is allowed to collide with ring
-    public static final CategoryFilter CARBON_FILTER = new CategoryFilter(CARBON_CATEGORY, Filters.MASK_ALL & ~512);
+    public static final CategoryFilter CARBON_FILTER = new CategoryFilter(CARBON_CATEGORY, Filters.MASK_ALL & ~Sink.SINK_CATEGORY);
 
     //Flying ring filter: collide with NOTHING
-    public static final CategoryFilter CARBON_FLYING_FILTER = new CategoryFilter(CARBON_CATEGORY, 0);
+    public static final CategoryFilter CARBON_FLYING_FILTER = new CategoryFilter(CARBON_CATEGORY, Sink.SINK_CATEGORY & ~CARBON_CATEGORY);
+    public static final CategoryFilter CARBON_CAPTURED_FILTER = new CategoryFilter(CAPTURED_CARBON_CATEGORY, Filters.MASK_ALL & ~CARBON_CATEGORY);
 
     public void initialize(){
         super.initialize();
@@ -141,9 +145,11 @@ public class Carbon extends VirtualGameElement {
         // Update the ringsOffField list if necessary (it keeps track of which rings are available to human player)
         if (status == Carbon.CarbonStatus.OFF_FIELD && !carbonsOffField.contains(this)) {
             carbonsOffField.add(this);
+            Compressor.capturedCarbons.remove(this);
         } else if (status != CarbonStatus.OFF_FIELD){
             carbonsOffField.remove(this);
         }
+
         // Set the linearDamping and the collision filter corresponding to the new status.
         body.setLinearDamping(status.getLinearDamping());
         carbonFixture.setFilter(status.getFilter());
